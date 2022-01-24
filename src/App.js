ffeb9c8b3b693b3a2a04137ee084909260751b36
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-01-24 14:19:42 +0530

    added some functionality and improve design

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=23802701-084d8c7ec2f95769998b17214&q=${term}&image_type=photo&pretty=true`)
       .then((response) => response.json())
-      .then((data) => setImg(data.hits))
+      .then((data) => {
+        setImg(data.hits);
+      })
       .catch((err) => console.log(err));
     // setColor("indigo");
   }, [term]);
@@ -23,7 +25,7 @@ function App() {
         <div className="mx-auto text-center">
           <SearchImg searchText={(txt) => setTerm(txt)} />
         </div>
-        <div className="grid grid-flow-row grid-cols-1 justify-items-center sm:grid-cols-2 md:grid-cols-3">
+        <div className="grid w-full grid-flow-row grid-cols-1 mx-auto sm:w-4/5 justify-items-center sm:grid-cols-2 md:grid-cols-3">
           {img.map((image) => (
             <Card key={image.id} image={image} />
           ))}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import "../assets/tailwind.css";
 import Items from "./Items";
 
 function Card({ image }) {
-  let tags = image.tags.split(",");
+  // const [stopLoading, setStopLoading] = useState(false);
+  let tags = image.tags.split(", ");
 
   return (
-    <div className={`w-[80%] sm:w-[95%] mb-8 bg-purple-100`}>
-      <img src={image.webformatURL} alt="" />
+    <div className={`w-[80%] sm:w-[95%] mb-8 rounded-md bg-purple-100 overflow-hidden`}>
+      <div className="w-full max-h-[16rem] overflow-hidden">
+        <img src={image.webformatURL} id="img" className="w-full duration-500 hover:scale-110" alt="" />
+      </div>
       <div className="flex flex-col items-center justify-center p-4">
-        {image.user !== "Photo-View" ? <h2 className={`text-3xl sm:text-xl text-center text-purple-700`}>Photo By {image.user}</h2> : <h2 className={`text-3xl text-purple-700`}>Photo By Me</h2>}
+        {image.user !== "Photo-View" ? <h2 className={`text-2xl sm:text-3xl text-center text-purple-700`}>Photo By {image.user}</h2> : <h2 className={`text-3xl text-purple-700`}>Photo By Me</h2>}
         <div className="flex flex-row">
           <Items image={image} />
         </div>
+        <div className="flex flex-row items-center justify-center">
+          <a href={image.pageURL} rel="noreferrer" target="_blank" className="mx-2 text-white bg-purple-700 rounded-md jellyBtn">
+            Get More
+          </a>
+          <a href={image.largeImageURL} download className="mx-2 text-white bg-purple-700 rounded-md jellyBtn">
+            Download
+          </a>
+        </div>
+        {/* <div className="loading"></div> */}
         <div className="flex flex-row flex-wrap items-center justify-center text-sm">
           {tags.map((hashTag, index) => (
             <button key={index} className={`my-2 p-2 text-purple-700 mx-0.5 border-[2px] bg-purple-50 border-purple-700 rounded-full`}>
