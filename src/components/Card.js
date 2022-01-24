6ff10394d8bec32b0635de60faca0550a9910c81
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-01-24 15:17:31 +0530

    remove a unusing module from import

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "../assets/tailwind.css";
